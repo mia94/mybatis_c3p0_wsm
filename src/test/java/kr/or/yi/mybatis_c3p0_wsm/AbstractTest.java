9e9ed337330b213e9767a6ebe10c8537214043ec
mia94 <mia_3@hanmail.net>
2018-11-28 14:47:35 +0900

    insertTest완료

--- src/test/java/kr/or/yi/mybatis_c3p0_wsm/AbstractTest.java
+++ src/test/java/kr/or/yi/mybatis_c3p0_wsm/AbstractTest.java
@@ -1,11 +1,8 @@
 package kr.or.yi.mybatis_c3p0_wsm;
 
-import static org.junit.Assert.*;
-
 import org.apache.ibatis.logging.Log;
 import org.apache.ibatis.logging.LogFactory;
 import org.junit.After;
-import org.junit.Test;
 
 public class AbstractTest {
 	protected static final Log log = LogFactory.getLog(AbstractTest.class);
@@ -13,11 +10,6 @@ public class AbstractTest {
 	@After
 	public void tearDown() throws Exception {
 		System.out.println();
-	}
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
 	}
 
 }
